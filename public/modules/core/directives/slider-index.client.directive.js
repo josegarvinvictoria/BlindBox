6107d76d106336479042a0c48b8b35a393f07321
jose <josegarvinvictoria@gmail.com>
2015-02-20 14:56:55 +0100

    Slider funcionant

---
+++ public/modules/core/directives/slider-index.client.directive.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('core').directive('sliderIndex', [
+
+    function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'modules/core/views/slider.html',
+            link: function postLink(scope, element, attrs) {
+                // Slider index directive logic
+                // ...
+                console.log(element[0]);
+                jQuery('.autoplay').slick({
+                    slidesToShow: 3,
+                   slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 100
+                    //centerMode: true,
+                    //centerPadding: '100px',
+                    //dots: true
+                });
+                //element.text('this is the sliderIndex directive');
+            }
+        };
+    }
+]);
